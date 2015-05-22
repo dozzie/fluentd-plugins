92f60cea4447265f30d27da2479138e3e023bc1a
Stanislaw Klekot <dozzie@jarowit.net>
2015-05-22 19:53:48 +0200

    Added support for transformation class.
    This should allow to preserve some context, e.g. data caching.

--- plugin/out_xform.rb
+++ plugin/out_xform.rb
@@ -10,6 +10,7 @@
 #   type xform
 #   xform_script /etc/fluent/xform/script.rb
 #   #xform_function process  # optional if xform_script has only one function
+#   #xform_class ClassName   # alternative to xform_function
 #
 #   # if no <store> defined, all messages are submitted back to the engine
 #   <store log.**>
@@ -35,6 +36,25 @@
 #
 # Note that if the `transform()' raises an exception, the message is lost.
 #
+# For `xform_class', class definition is as follows:
+#
+# class ClassName
+#   def initialize()
+#     # any necessary initialization; NOTE: network connections are poor
+#     # choice to put here
+#   end
+#
+#   def destroy()
+#     # called on plugin shutdown
+#   end
+#
+#   def transform(time, tag, record)
+#     # the description of `transform()' function applies here, except that
+#     # the method needs to be named "transform"
+#     yield ...
+#   end
+# end
+#
 #-----------------------------------------------------------------------------
 
 class XForm < Fluent::Output
@@ -42,6 +62,7 @@ class XForm < Fluent::Output
 
   config_param :xform_script, :string
   config_param :xform_function, :string, :default => nil
+  config_param :xform_class, :string, :default => nil
   # also: <store></store> elements
 
   #unless method_defined?(:log)
@@ -49,8 +70,10 @@ class XForm < Fluent::Output
   #end
 
   def initialize
-    @xform_function = nil # filename
-    @xform_script = nil   # function name
+    @xform_script = nil   # filename
+    @xform_function = nil # function name
+    @xform_class = nil    # class name
+    @xform_instance = nil # object
     @xform = nil          # callable object
 
     @matches = []
@@ -64,15 +87,29 @@ class XForm < Fluent::Output
 
     # config_param() calls didn't work and I don't know why; they're left as
     # a reference
-    @xform_function = conf["xform_function"]
     @xform_script   = conf["xform_script"]
+    @xform_function = conf["xform_function"]
+    @xform_class    = conf["xform_class"]
 
-    functions = load_functions(@xform_script)
+    (functions, classes) = load_script(@xform_script)
+
+    $log.debug "loaded script", :script => @xform_script
 
     if @xform_function
+      if @xform_class
+        raise ConfigError, "`xform_function' and `xform_class' specified at the same time"
+      end
+      $log.debug "transform function specified", :function => @xform_function
       @xform = functions[@xform_function.to_sym]
+    elsif @xform_class
+      # TODO: pass `conf' to the constructor
+      # TODO: catch errors (missing class, mainly)
+      $log.debug "transform class specified", :class => @xform_class
+      @xform_instance = classes[@xform_class.to_sym].new
+      @xform = @xform_instance.method(:transform)
     elsif functions.length == 1
-      @xform_function, @xform = functions.shift
+      (@xform_function, @xform) = functions.shift
+      $log.debug "transform function autodetected", :function => @xform_function
     else
       if @xform_function != nil
         raise ConfigError, "Function #{@xform_function} not loaded from script #{@xform_script}"
@@ -119,6 +156,17 @@ class XForm < Fluent::Output
   end
 
   def shutdown
+    if @xform_instance
+      begin
+        @xform_instance.destroy
+      rescue => e
+        $log.warn "unexpected error while shutting down",
+                  :xform_class => @xform_instance.class,
+                  :error_class => e.class, :error => e
+        $log.warn_backtrace
+      end
+    end
+
     @matches.each {|m|
       begin
         m.shutdown
@@ -162,15 +210,26 @@ class XForm < Fluent::Output
   end
 
   # load functions from file
-  def load_functions(file)
+  def load_script(file)
     context = Object.new
     new_methods = []
     context.define_singleton_method(:singleton_method_added) {|id|
       new_methods << id unless id == :singleton_method_added # skip myself
     }
 
+    # context.singleton_class::Nabla.new()
+    # context.singleton_class.constants
+    #   => [:Nabla]
+    # context.singleton_class.const_get(:Nabla).new()
+
     context.instance_eval File.read(file)
-    return Hash[ new_methods.map{|m| [m, context.method(m)]} ]
+
+    functions = Hash[ new_methods.map{|m| [m, context.method(m)]} ]
+
+    klass = context.singleton_class
+    classes = Hash[ klass.constants.map{|k| [k, klass.const_get(k)]} ]
+
+    return [functions, classes]
   end
 
   # helper to emit events back to Fluent::Engine
